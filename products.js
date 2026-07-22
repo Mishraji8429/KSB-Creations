7998ef71514e5bd6a8fb7d9e6bfc84b3cbb0401e
Mishraji8429 <ashukuma222@gmail.com>
2026-07-22 19:51:09 +0530

    Add product list to products.js
    Added an array of product objects with details including name, price, image, and link.

--- products.js
+++ products.js
@@ -1,1 +1,31 @@
+const products = [
 
+{
+name:"Premium Kurti",
+price:"₹499",
+image:"images/kurti.jpg",
+link:"https://www.meesho.com/"
+},
+
+{
+name:"Fashion Jewellery",
+price:"₹299",
+image:"images/jewellery.jpg",
+link:"https://www.meesho.com/"
+},
+
+{
+name:"Women's Hand Bag",
+price:"₹699",
+image:"images/bag.jpg",
+link:"https://www.meesho.com/"
+},
+
+{
+name:"Hair Curler",
+price:"₹999",
+image:"images/curler.jpg",
+link:"https://www.meesho.com/"
+}
+
+];
