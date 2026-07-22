6066b1ed2b2e7a77cd4a4aaa47bb96baa24b7bdf
Mishraji8429 <ashukuma222@gmail.com>
2026-07-22 19:49:44 +0530

    Implement loader fade-out effect on page load
    Add a loader that fades out on page load

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
 
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 500);
+});
