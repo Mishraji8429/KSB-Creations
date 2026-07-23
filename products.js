c7782d0e76d00c8926f48564007aea6403b75dac
Mishraji8429 <ashukuma222@gmail.com>
2026-07-23 16:46:35 +0530

    Modify product prices and update links
    Updated prices and links for several products.

--- products.js
+++ products.js
@@ -9,23 +9,23 @@ link:"https://www.meesho.com/s/p/gqfkha?utm_source=s_cc"
 
 {
 name:"Fashion Jewellery",
-price:"₹299",
+price:"₹229",
 image:"images/jewellery.jpg",
-link:"https://www.meesho.com/"
+link:"https://www.meesho.com/s/p/gtbtfi?utm_source=s_cc"
 },
 
 {
 name:"Women's Hand Bag",
-price:"₹699",
+price:"₹239",
 image:"images/bag.jpg",
-link:"https://www.meesho.com/"
+link:"https://www.meesho.com/s/p/gnlirv?utm_source=s_cc"
 },
 
 {
 name:"Hair Curler",
-price:"₹999",
+price:"₹339",
 image:"images/curler.jpg",
-link:"https://www.meesho.com/"
+link:"https://www.meesho.com/s/p/groawz?utm_source=s_cc"
 }
 
 ];
