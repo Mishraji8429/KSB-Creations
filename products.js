ea2ac31f90393798cb790c451cc9ae4aa01493e7
Mishraji8429 <ashukuma222@gmail.com>
2026-07-23 16:51:44 +0530

    Fix syntax errors in products.js

--- products.js
+++ products.js
@@ -3,6 +3,9 @@ const products = [
 {
 name:"Premium Kurti",
 price:"₹239",
+oldPrice:"₹499",
+rating:"4.8",
+badge:"Best Seller",
 image:"images/kurti.jpg",
 link:"https://www.meesho.com/s/p/gqfkha?utm_source=s_cc"
 },
@@ -10,6 +13,9 @@ link:"https://www.meesho.com/s/p/gqfkha?utm_source=s_cc"
 {
 name:"Fashion Jewellery",
 price:"₹229",
+oldPrice:"₹399",
+rating:"4.9",
+badge:"Trending",
 image:"images/jewellery.jpg",
 link:"https://www.meesho.com/s/p/gtbtfi?utm_source=s_cc"
 },
@@ -17,6 +23,9 @@ link:"https://www.meesho.com/s/p/gtbtfi?utm_source=s_cc"
 {
 name:"Women's Hand Bag",
 price:"₹239",
+oldPrice:"₹499",
+rating:"4.7",
+badge:"Hot Deal",
 image:"images/bag.jpg",
 link:"https://www.meesho.com/s/p/gnlirv?utm_source=s_cc"
 },
@@ -24,6 +33,9 @@ link:"https://www.meesho.com/s/p/gnlirv?utm_source=s_cc"
 {
 name:"Hair Curler",
 price:"₹339",
+oldPrice:"₹599",
+rating:"4.8",
+badge:"New Arrival",
 image:"images/curler.jpg",
 link:"https://www.meesho.com/s/p/groawz?utm_source=s_cc"
 }
