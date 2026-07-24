bda3ae3ac73f59f1b67b3301a77ae51473cd0394
Mishraji8429 <ashukuma222@gmail.com>
2026-07-24 22:20:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,20 @@ setInterval(() => {
   current = (current + 1) % heroImages.length;
   hero.src = heroImages[current];
 }, 4000);
+const search = document.getElementById("search");
+
+search.addEventListener("keyup", function () {
+
+const value = search.value.toLowerCase();
+
+const cards = document.querySelectorAll(".product-card");
+
+cards.forEach(card => {
+
+const text = card.innerText.toLowerCase();
+
+card.style.display = text.includes(value) ? "block" : "none";
+
+});
+
+});
