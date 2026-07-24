fdce577d3c6f00ab7c43fc3598cb764e6a1c34ac
Mishraji8429 <ashukuma222@gmail.com>
2026-07-24 22:24:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,3 +78,19 @@ card.style.display = text.includes(value) ? "block" : "none";
 });
 
 });
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
